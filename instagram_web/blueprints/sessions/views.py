1961587dc12899f42f1bdee06361577db4981158
sunnyyap1011 <sunnyyap1011@gmail.com>
2019-08-27 19:26:03 +0800

    Import users_blueprint and Modify route to login and logout

--- instagram_web/blueprints/sessions/views.py
+++ instagram_web/blueprints/sessions/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash, session
 from app import login_manager
+from instagram_web.blueprints.users.views import users_blueprint
 from flask_login import login_user, logout_user, login_required
 from werkzeug.security import check_password_hash
 from models.user import User
@@ -10,12 +11,12 @@ sessions_blueprint = Blueprint('sessions',
                             template_folder='templates')
 
                             
-@sessions_blueprint.route('/new', methods=['GET'])
-def new():
+@users_blueprint.route('/login', methods=['GET'])
+def login():
     return render_template('sessions/new.html')
 
 
-@sessions_blueprint.route('/', methods=['POST'])
+@sessions_blueprint.route('/login', methods=['POST'])
 def create():
     user = User.get_or_none(username=request.form.get('username'))
 
@@ -39,9 +40,9 @@ def create():
     return render_template('sessions/new.html')
 
 
-@sessions_blueprint.route('/logout')
+@users_blueprint.route('/logout')
 @login_required
 def destroy():
     logout_user()
     flash("You've been logged out successfully", 'info')
-    return redirect(url_for('sessions.new'))
+    return redirect(url_for('users.login'))
