611bfaf1b163afe28a127cc863bb352770336cef
sunnyyap1011 <sunnyyap1011@gmail.com>
2019-08-28 16:01:41 +0800

    Added Edit user profile template and validate data when update

--- instagram_web/blueprints/users/views.py
+++ instagram_web/blueprints/users/views.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash, session
 from app import login_manager
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.security import check_password_hash
 from models.user import User
+
 
 
 users_blueprint = Blueprint('users',
@@ -10,7 +11,7 @@ users_blueprint = Blueprint('users',
                             template_folder='templates')
 
 
-@users_blueprint.route('/new', methods=['GET'])
+@users_blueprint.route('/sign_up', methods=['GET'])
 def new():
     return render_template('users/new.html')
 
@@ -24,7 +25,7 @@ def create():
     u = User(username=username, password=password, email=email)
 
     if u.save():
-        flash(f'{u.username} successfully created', 'success')
+        flash(f"{u.username}, you've sign up successfully, please login to view your profile", 'success')
         return redirect(url_for('users.new'))
     else:
         flash(f'{u.errors[0]}', 'danger')
@@ -43,13 +44,54 @@ def index():
 
 
 @users_blueprint.route('/<id>/edit', methods=['GET'])
+@login_required
 def edit(id):
-    pass
+    return render_template('users/edit.html', id=current_user.id)
 
 
 @users_blueprint.route('/<id>', methods=['POST'])
 def update(id):
-    pass
+    new_username = request.form.get('username')
+    new_email = request.form.get('email')
+    current_password = request.form.get('current_password')
+    new_password = request.form.get('new_password')
+
+    user = User.get_by_id(id)
+
+
+    if current_user == user:
+        if new_username == user.username and new_email == user.email and not current_password and not new_password:
+            flash("No change made to the user profile", 'info')
+            return redirect(url_for('users.edit', id=id))
+
+        if new_email != user.email:
+            user.email = new_email
+
+        if new_username != user.username:
+            user.username = new_username
+
+        if current_password and new_password:
+            if check_password_hash(user.password, current_password):
+                user.password = new_password
+
+            else:
+                flash('Invalid current password', 'danger')
+                return redirect(url_for('users.edit', id=id))
+
+        if user.save():
+            flash("Your user profile have been updated successfully", 'success')
+            return redirect(url_for('users.show', username=user.username))
+        else:
+            flash(f"{user.errors[0]}", 'danger')
+            return redirect(url_for('users.edit', id=id))
+
+
+
+    else:
+        return redirect(url_for('users.login'))
+
+    return redirect(url_for('users.show', username=user.username))
+
 
 
 @login_manager.user_loader
--- models/user.py
+++ models/user.py
@@ -1,8 +1,8 @@
 from models.base_model import BaseModel
-from werkzeug.security import generate_password_hash
-from flask_login import UserMixin
-import peewee as pw
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin, current_user
 import re
+import peewee as pw
 
 class User(BaseModel, UserMixin):
     username = pw.CharField(unique=True)
@@ -15,16 +15,27 @@ class User(BaseModel, UserMixin):
         email_regex = "^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$"
         password_regex = "^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,15}$"
 
-
-        if duplicate_users:
+        if not self.id and duplicate_users:
             self.errors.append('Username exists')
-        if duplicate_email:
+        if self.id and self.username != current_user.username and duplicate_users:
+            self.errors.append('Username exists')    
+
+        if not self.id and duplicate_email:
             self.errors.append('Email exists')
+        if self.id and self.email != current_user.email and duplicate_email:
+            self.errors.append('Email exists')    
+
         if not re.match(email_regex, self.email):
             self.errors.append('Please enter a valid email address')
-        if not re.match(password_regex, self.password):
-            self.errors.append('Password must be at least 6 characters, no more than 15 characters, and must include at least one upper case letter, one lower case letter, and one numeric digit.')
-        if not self.id:
-            self.password = generate_password_hash(self.password)
-
             
+        if self.id and self.password != current_user.password:
+            if not re.match(password_regex, self.password):
+                self.errors.append('New password must be at least 6 characters, no more than 15 characters, and must include at least one upper case letter, one lower case letter, and one numeric digit.')
+            else:    
+                self.password = generate_password_hash(self.password)
+
+        if not self.id:
+            if not re.match(password_regex, self.password):
+                self.errors.append('Password must be at least 6 characters, no more than 15 characters, and must include at least one upper case letter, one lower case letter, and one numeric digit.')
+            else:
+                self.password = generate_password_hash(self.password)
